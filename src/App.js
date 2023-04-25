4448d7352198fb21969b68fe9ff541c9040f49f6
Taimur Mumtaz <taimur.mumtaz0@gmail.com>
2023-04-25 13:08:49 -0400

    showcase component + global styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { Theme } from "./Utils/Theme";
+import { Container, MainBody } from "./Styles/Global.styled";
+import Showcase from "./Components/Showcase";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+     <MainBody>
+      <Container>
+        <Showcase />
+      </Container>
+     </MainBody>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Components/Showcase.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+import {
+  PaddingContainer,
+  FlexContainer,
+  Heading,
+  ParaText,
+  BlueText,
+  IconContainer,
+} from "../Styles/Global.styled";
+
+import { ShowcaseParticleContainer, ShowcaseImageCard, Particle } from "../Styles/Showcase.styled";
+
+import { BsLinkedin, BsGithub } from 'react-icons/bs'
+
+import ShowcaseImage from '../Assets/showcase-img.png'
+import BackgroundImg from '../Assets/particle.png'
+
+const Showcase = () => {
+  return (
+    <PaddingContainer id="Home" left="3%" right="10%" top="15%" bottom="10%">
+      <FlexContainer align="center" fullWidthChild>
+        
+        <div>
+          <Heading as="h1" size="h1">
+            Taimur Mumtaz
+          </Heading>
+
+          <Heading as="h2" size="h2" top="0.5rem" bottom="1rem">
+            Front End Developer <BlueText>UX/UI Designer</BlueText>
+          </Heading>
+
+      
+
+         {/*-- Social Icons-- */}
+          <FlexContainer  gap="40px">
+            <IconContainer  size="2rem">
+                <BsLinkedin color="white" />
+            </IconContainer>
+
+            <IconContainer size="2rem">
+                <BsGithub color="white" />
+            </IconContainer>
+          </FlexContainer>
+        </div>
+
+        <FlexContainer justify="flex-end">
+            <ShowcaseParticleContainer>
+                <ShowcaseImageCard>
+                    <img src={ShowcaseImage} alt="Showcase" />
+                </ShowcaseImageCard>
+                <Particle src={BackgroundImg} alt="Particle" top="-80px" left="20px" rotate="60deg" />
+                <Particle src={BackgroundImg} alt="Particle" top="50px" right="-70px" rotate="0deg" />
+                <Particle src={BackgroundImg} alt="Particle" bottom="10px" left="-70px" rotate="50deg" />
+            </ShowcaseParticleContainer>
+        </FlexContainer>
+      </FlexContainer>
+    </PaddingContainer>
+  );
+};
+
+export default Showcase;
